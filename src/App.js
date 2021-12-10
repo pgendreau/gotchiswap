54f559eeac51022903a4b762fea946a86cf84e02
Philippe Gendreau <pgendreau@gmail.com>
2021-12-09 23:30:16 -0500

    main page

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
+import { useEffect } from 'react';
 import './App.css';
-import { useState } from 'react';
-import { ethers } from 'ethers'
-import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 
-// Update with the contract address logged out to the CLI when it was deployed 
-const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+import contract from './artifacts/contracts/Escrow.sol/Escrow.json';
+
+const contractAddress = "0xBef4c6C2c5Fed6B1d19c1508f955Cb39E2383C4f";
+const abi = contract.abi;
 
 function App() {
-  // store greeting in local state
-  const [greeting, setGreetingValue] = useState()
 
-  // request access to the user's MetaMask account
-  async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const checkWalletIsConnected = () => { }
+
+  const connectWalletHandler = () => { }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
   }
 
-  // call the smart contract, read the current greeting value
-  async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-      } catch (err) {
-        console.log("Error: ", err)
-      }
-    }    
-  }
-
-  // call the smart contract, send an update
-  async function setGreeting() {
-    if (!greeting) return
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(greeting)
-      await transaction.wait()
-      fetchGreeting()
-    }
-  }
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
-      </header>
+    <div className='main-app'>
+      <h1>GotchiSwap main page</h1>
+      <div>
+        {connectWalletButton()}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
