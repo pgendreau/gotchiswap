17ce5c6dea210488ab371cc410c5f4f6d2ad5d7b
Philippe Gendreau <pgendreau@gmail.com>
2021-12-25 09:59:05 -0500

    add connect wallet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { ethers } from 'ethers';
 
 import contract from './artifacts/contracts/Escrow.sol/Escrow.json';
 
@@ -8,9 +9,45 @@ const abi = contract.abi;
 
 function App() {
 
-  const checkWalletIsConnected = () => { }
+  const [currentAccount, setCurrentAccount] = useState(null);
 
-  const connectWalletHandler = () => { }
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
 
   const connectWalletButton = () => {
     return (
