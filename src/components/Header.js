c5f422f210772b83c4fa72eebf5fedbe4ab09975
Philippe Gendreau <pgendreau@gmail.com>
2022-01-09 13:56:03 -0500

    add logo

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Gotchiswap Logo" />
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import {
   Route,
 } from "react-router-dom";
 
+import Layout from '../components/Layout';
+
 import Main from './main';
 import Buy from './buy';
 import Sell from './sell';
@@ -13,11 +15,13 @@ import Sell from './sell';
 const Index = () => {
   return(
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Main />}></Route>
-        <Route path="/buy" element={<Buy />}></Route>
-        <Route path="/sell" element={<Sell />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Main />}></Route>
+          <Route path="/buy" element={<Buy />}></Route>
+          <Route path="/sell" element={<Sell />}></Route>
+        </Routes>
+      </Layout>
     </Router>
   );
 };
