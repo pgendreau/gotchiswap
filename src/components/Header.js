66ba476c4aa212b3723ef8ad07d6a902d3445124
Philippe Gendreau <pgendreau@gmail.com>
2022-01-10 12:51:34 -0500

    center logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,11 @@ import React from 'react';
 
 import logo from '../images/logo.png';
 
-//const connectWalletPressed = async () => {
-//    const walletResponse = await connectWallet();
-//    setStatus(walletResponse.status);
-//    setWallet(walletResponse.address);
-//};
-
 const Header = () => {
   return (
-    <header>
+    <div class="header">
       <img src={logo} alt="Gotchiswap Logo" />
-    </header>
+    </div>
   );
 };
 
